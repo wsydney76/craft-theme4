365f5e68aad51db1def9572d7e5ea644abc0f822
wsydn <wsydney@web.de>
2022-01-13 22:28:38 +0100

    one|all filters

--- modules/main/twigextensions/TwigExtension.php
+++ modules/main/twigextensions/TwigExtension.php
@@ -2,17 +2,49 @@
 
 namespace modules\main\twigextensions;
 
-
 use Craft;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
 
 class TwigExtension extends AbstractExtension implements GlobalsInterface
 {
     public function getGlobals(): array
     {
         return [
-          'theme' => Craft::$app->config->getConfigFromFile('theme')
+            'theme' => Craft::$app->config->getConfigFromFile('theme')
         ];
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('one', [$this, 'oneFilter']),
+            new TwigFilter('all', [$this, 'allFilter']),
+        ];
+    }
+
+    public function oneFilter($stuff)
+    {
+        if (is_null($stuff)) {
+            return null;
+        }
+        if (is_array($stuff)) {
+            return (count($stuff) ? $stuff[0] : null);
+        }
+        return $stuff->one();
+    }
+
+    public function allFilter($stuff)
+    {
+        if (is_null($stuff))
+        {
+            return [];
+        }
+        if (is_array($stuff))
+        {
+            return ($stuff);
+        }
+        return $stuff->all();
     }
 }
