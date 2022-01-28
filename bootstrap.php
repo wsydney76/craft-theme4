d9e0b4ea8a49eccac04c067f70b6516e8a1f6b15
wsydn <wsydney@web.de>
2022-01-28 12:24:46 +0100

    Shared bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+use config\Env;
+
+// Set path constants
+define('CRAFT_BASE_PATH', __DIR__);
+define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
+define('CRAFT_TEMPLATES_PATH', CRAFT_BASE_PATH.'/templates');
+
+// Load Composer's autoloader
+require_once CRAFT_VENDOR_PATH . '/autoload.php';
+
+define('CRAFT_ENVIRONMENT', Env::ENVIRONMENT ?: 'production');
