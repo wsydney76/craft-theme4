3b86d2c5b42051dfd15f1a5d2de86f7152bbd0f8
wsydn <wsydney@web.de>
2022-06-01 11:00:07 +0200

    Rework create author entry for user

--- modules/main/controllers/ContentController.php
+++ modules/main/controllers/ContentController.php
@@ -3,14 +3,21 @@
 namespace modules\main\controllers;
 
 use Craft;
+use craft\base\Element;
+use craft\base\ElementInterface;
+use craft\elements\Entry;
 use craft\helpers\App;
+use craft\helpers\ElementHelper;
 use craft\web\Controller;
 use craft\web\Response;
 use modules\main\MainModule;
 use modules\main\models\MessageModel;
 use yii\web\BadRequestHttpException;
+use yii\web\ServerErrorHttpException;
 use function Arrayy\array_last;
 use function explode;
+use function implode;
+use function sprintf;
 
 class ContentController extends Controller
 {
@@ -71,5 +78,40 @@ class ContentController extends Controller
 
         $this->setSuccessFlash(Craft::t('site', 'Message sent'));
         return $this->redirectToPostedUrl((object)['id' => $id]);
+    }
+
+    public function actionCreateAuthorPersonEntry(): \yii\web\Response
+    {
+        $data = $this->request->getBodyParams();
+
+        $element = new Entry([
+            'sectionId' => $data['sectionId'],
+            'typeId' => $data['typeId'],
+            'siteId' => $data['siteId'],
+            'authorId' => $data['authorId'],
+        ]);
+        $element->setFieldValue('user', [$data['userId']]);
+        $element->setFieldValue('email', $data['email']);
+        $element->setFieldValue('firstName', $data['firstName']);
+        $element->setFieldValue('lastName', $data['lastName']);
+
+        if (!$element->slug) {
+            $element->slug = ElementHelper::tempSlug();
+        }
+
+        $element->setScenario(Element::SCENARIO_ESSENTIALS);
+        if (!Craft::$app->getDrafts()->saveElementAsDraft($element, $data['authorId'], null, null, false)) {
+            throw new ServerErrorHttpException(sprintf('Unable to save draft: %s', implode(', ', $element->getErrorSummary(true))));
+        }
+
+        return $this->_asSuccess(Craft::t('app', '{type} created.', [
+            'type' => Craft::t('app', 'Draft'),
+        ]), $element);
+    }
+
+    // From ElementsController, copied here because it's private
+    private function _asSuccess(string $message, ElementInterface $element, array $data = [], bool $addAnother = false): \yii\web\Response
+    {
+        return $this->asModelSuccess($element, $message, 'element', $data);
     }
 }
